d92fdcbe25fad370b600201f0ce4f8be80a2c768
Alex Wolf <alexrudiwolf@gmail.com>
2022-02-19 16:59:22 -0500

    progress

--- WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace WiredBrainCoffeeAdmin.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        
+        public List<SurveyItem> SurveyItems { get; set; }
+        
+        [BindProperty]
+        public string HelpText { get; set; }
 
         public IndexModel(ILogger<IndexModel> logger)
         {
@@ -14,7 +25,13 @@ namespace WiredBrainCoffeeAdmin.Pages
 
         public void OnGet()
         {
+            string text = System.IO.File.ReadAllText("wwwroot/SampleData/survey.json");
+            SurveyItems = JsonSerializer.Deserialize<List<SurveyItem>>(text);
+        }
 
+        public class SurveyItem {
+            public string Question { get; set; }
+            public string Answer { get; set; }
         }
     }
 }
