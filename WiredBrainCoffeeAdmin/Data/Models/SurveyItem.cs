d4c7f33001cc0ad5f1098176dc058f76ce80a29b
Alex Wolf <alexwolf@mcirosoft.com>
2022-03-06 14:43:56 -0500

    progress

---
+++ WiredBrainCoffeeAdmin/Data/Models/SurveyItem.cs
@@ -0,0 +1,8 @@
+﻿namespace WiredBrainCoffeeAdmin.Data.Models
+{
+    public class SurveyItem
+    {
+        public string Question { get; set; }
+        public string Answer { get; set; }
+    }
+}
--- WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
+++ WiredBrainCoffeeAdmin/Pages/Index.cshtml.cs
@@ -6,38 +6,25 @@ using System.Text.Json;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using WiredBrainCoffeeAdmin.Data.Models;
 
 namespace WiredBrainCoffeeAdmin.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
-        private readonly IHttpClientFactory _client;
 
         public List<SurveyItem> SurveyItems { get; set; } = new List<SurveyItem>();
-        
-        [BindProperty]
-        public string HelpText { get; set; }
 
-        public string HelpResponse { get; set; }
-
-        public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory client)
+        public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
-            _client = client;
         }
 
         public void OnGet()
         {
             string text = System.IO.File.ReadAllText("wwwroot/SampleData/survey.json");
             SurveyItems = JsonSerializer.Deserialize<List<SurveyItem>>(text);
-        }
-
-        
-
-        public class SurveyItem {
-            public string Question { get; set; }
-            public string Answer { get; set; }
         }
     }
 }
