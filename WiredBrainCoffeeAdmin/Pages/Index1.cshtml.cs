57e5e867cfaa0ba6173482ee55e878fcb113c637
Alex Wolf <alexrudiwolf@gmail.com>
2022-03-08 18:02:47 -0500

    progress

---
+++ WiredBrainCoffeeAdmin/Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WiredBrainCoffeeAdmin.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
