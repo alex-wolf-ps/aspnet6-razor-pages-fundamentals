80cb351599e95a791a88129768521b2fff72df8a
Alex Wolf <alexwolf@mcirosoft.com>
2022-03-29 12:09:40 -0400

    end

--- WiredBrainCoffeeAdmin/Data/Models/Product.cs
+++ WiredBrainCoffeeAdmin/Data/Models/Product.cs
@@ -7,14 +7,17 @@ namespace WiredBrainCoffeeAdmin.Data
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
-
+        [MaxLength(30)]
+        [Required]
         public string ShortDescription { get; set; }
-
+        [MinLength(20)]
+        [Required]
         public string Description { get; set; }
-
+        [Required]
         public decimal Price { get; set; }
-
+        [Required]
         public string Category { get; set; }
 
         public IFormFile Upload { get; set; }
