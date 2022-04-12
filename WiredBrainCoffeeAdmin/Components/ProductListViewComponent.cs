2d94ae98fcc6d2f9534f6f80b9efdf840484f2ec
Alex Wolf <alexwolf@mcirosoft.com>
2022-04-12 16:53:46 -0400

    end

---
+++ WiredBrainCoffeeAdmin/Components/ProductListViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WiredBrainCoffeeAdmin.Data;
+
+namespace WiredBrainCoffeeAdmin.Components
+{
+    public class ProductListViewComponent : ViewComponent
+    {
+        private IProductRepository productRepo;
+
+        public ProductListViewComponent(IProductRepository productRepository)
+        {
+            this.productRepo = productRepository;
+        }
+
+        public IViewComponentResult Invoke(int count)
+        {
+            var items = productRepo.GetAll();
+            if(count > 0)
+            {
+                return View(items.Take(count).ToList());
+            }
+            
+            return View(items);
+        }
+    }
+}
--- WiredBrainCoffeeAdmin/Program.cs
+++ WiredBrainCoffeeAdmin/Program.cs
@@ -11,12 +11,15 @@ builder.Services.AddDbContext<WiredContext>(options =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 var app = builder.Build();
 
+await CreateDb(app.Services, app.Logger);
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    //app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    //context.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
@@ -30,3 +33,9 @@ app.UseAuthorization();
 app.MapRazorPages();
 
 app.Run();
+
+async Task CreateDb(IServiceProvider services, ILogger logger)
+{
+    using var db = services.CreateScope().ServiceProvider.GetRequiredService<WiredContext>();
+    await db.Database.MigrateAsync();
+}
