fc82c80fd90914e6b6b13468fd7cac38f46cbb09
Alex Wolf <alexwolf@mcirosoft.com>
2022-04-20 17:41:59 -0400

    start

--- WiredBrainCoffeeAdmin/Program.cs
+++ WiredBrainCoffeeAdmin/Program.cs
@@ -5,10 +5,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
 builder.Services.AddDbContext<WiredContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("WiredBrain")));
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
