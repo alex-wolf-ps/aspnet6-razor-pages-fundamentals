e61ef950f5f73d8378ee7a6f7f9b2878e72bd029
Alex Wolf <alexwolf@mcirosoft.com>
2022-04-24 21:27:28 -0400

    start

--- WiredBrainCoffeeAdmin/Data/Models/Product.cs
+++ WiredBrainCoffeeAdmin/Data/Models/Product.cs
@@ -3,31 +3,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WiredBrainCoffeeAdmin.Data
 {
-    public class MenuItem
+    public class Product
     {
         public int Id { get; set; }
 
-        [Required]
         public string Name { get; set; }
 
-        [Required]
-        public string Description { get; set; }
-
-        [Required]
         public string ShortDescription { get; set; }
 
-        [Required]
+        public string Description { get; set; }
+
         public decimal Price { get; set; }
 
-        public string ImgFileName { get; set; }
-
-        [Required]
-        public DateTime Created { get; set; }
-
-        [Required]
         public string Category { get; set; }
 
-        [NotMapped]
         public IFormFile Upload { get; set; }
+
+        public string ImageFileName { get; set; }
+
+        public DateTime Created { get; set; }
     }
 }
